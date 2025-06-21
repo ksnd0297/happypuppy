c9fbd9c48bf2a14d2f5f9dbda58528f1969306da
joy.hwan <joy.hwan@kakaocorp.com>
2025-06-22 00:10:53 +0900

    feat : 채팅 페이지 구성 및 다가오는 약속 컴포넌트 제작

--- happy-puppy-react-native/app/screens/ChatList.tsx
+++ happy-puppy-react-native/app/screens/ChatList.tsx
@@ -1,10 +1,27 @@
-import { StyleSheet, View } from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
+import Text from "../components/shared/Text";
+import UpComingReservation from "../components/chatList/UpComingReservation";
 
 const ChatListPage = () => {
   return (
     <View style={styles.container}>
-      <View>HEADER</View>
-      <View>UPCOMING RESERVATION</View>
+      <View style={styles.titleContainer}>
+        <Text xxxlarge bold>
+          약속 목록
+        </Text>
+      </View>
+      <View style={styles.upcomingReservationContainer}>
+        <Text xlarge bold>
+          다가오는 약속
+        </Text>
+        <ScrollView horizontal contentContainerStyle={styles.upcomingReservationList} showsHorizontalScrollIndicator={false}>
+          <UpComingReservation imageUri="https://fastly.picsum.photos/id/212/200/300.jpg?hmac=2PUnX8vk476_x3NwjUExdYhPxVyP1Qd17BLvvBYTONQ" title="동천역 강아지 산책" dateTime="2025. 6. 22. (일) 18:00" />
+          <UpComingReservation imageUri="https://fastly.picsum.photos/id/212/200/300.jpg?hmac=2PUnX8vk476_x3NwjUExdYhPxVyP1Qd17BLvvBYTONQ" title="동천역 강아지 산책" dateTime="2025. 6. 22. (일) 18:00" />
+          <UpComingReservation imageUri="https://fastly.picsum.photos/id/212/200/300.jpg?hmac=2PUnX8vk476_x3NwjUExdYhPxVyP1Qd17BLvvBYTONQ" title="동천역 강아지 산책" dateTime="2025. 6. 22. (일) 18:00" />
+          <UpComingReservation imageUri="https://fastly.picsum.photos/id/212/200/300.jpg?hmac=2PUnX8vk476_x3NwjUExdYhPxVyP1Qd17BLvvBYTONQ" title="동천역 강아지 산책" dateTime="2025. 6. 22. (일) 18:00" />
+          <UpComingReservation imageUri="https://fastly.picsum.photos/id/212/200/300.jpg?hmac=2PUnX8vk476_x3NwjUExdYhPxVyP1Qd17BLvvBYTONQ" title="동천역 강아지 산책" dateTime="2025. 6. 22. (일) 18:00" />
+        </ScrollView>
+      </View>
       <View>CHAT LIST</View>
       <View>FOOTER</View>
     </View>
@@ -19,7 +36,26 @@ const styles = StyleSheet.create({
     backgroundColor: "#FCF5EE",
   },
 
-  upcomingReservationContainer: {},
+  titleContainer: {
+    flex: 0.05,
+
+    padding: 10,
+  },
+
+  upcomingReservationContainer: {
+    flex: 0.2,
+
+    padding: 10,
+
+    gap: 10,
+
+    backgroundColor: "#FCF5EE",
+  },
+  upcomingReservationList: {
+    flexDirection: "row",
+
+    gap: 20,
+  },
 
   chatListContainer: {},
 
