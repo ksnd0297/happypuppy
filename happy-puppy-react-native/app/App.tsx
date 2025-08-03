12b07bc187bc0b1c63c292e0916a84d94ec3969f
joy.hwan <joy.hwan@kakaocorp.com>
2025-08-03 16:57:40 +0900

    feat : 화면 진입 시 토큰 체크 및 네비게이션 로직 제작

--- happy-puppy-react-native/app/App.tsx
+++ happy-puppy-react-native/app/App.tsx
@@ -85,10 +85,10 @@ export default function App() {
       <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
         <NavigationContainer>
           <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Login" component={LoginPage} />
             <Stack.Screen name="Register" component={RegisterPage} />
             <Stack.Screen name="Home" component={HomePage} />
             <Stack.Screen name="ChatList" component={ChatListPage} />
-            <Stack.Screen name="Login" component={LoginPage} />
             <Stack.Screen name="Chat" component={ChatPage} />
             <Stack.Screen name="Webview" component={DefaultWebviewScreen} />
           </Stack.Navigator>
--- happy-puppy-react-native/app/screens/Login.tsx
+++ happy-puppy-react-native/app/screens/Login.tsx
@@ -1,4 +1,4 @@
-import { isLogined, login, me } from "@react-native-kakao/user";
+import { isLogined, me } from "@react-native-kakao/user";
 import { useNavigation } from "@react-navigation/native";
 import { Image, Pressable, StyleSheet, Text, View } from "react-native";
 import { RootStackNavigationProp } from "../App";
@@ -31,12 +31,43 @@ const LoginPage = () => {
       }
       // * 회원가입이 되어있지 않은 경우
       else {
-      navigation.navigate("Register");
+        navigation.navigate("Register");
       }
     } catch (error) {
       console.error("Kakao login failed:", error);
     }
   };
+
+  useEffect(() => {
+    if (isLoading || !token) return;
+
+    const { accessToken, accessTokenExpiresAt } = token;
+
+    const isAccessTokenValid = accessToken && new Date(accessTokenExpiresAt * 1000).getTime() > new Date().getTime();
+
+    // * Access Token 이 존재하고 만료되지 않은 경우
+    if (accessToken && isAccessTokenValid) {
+      (async () => {
+        const isLoggedIn = await isLogined();
+
+        if (!isLoggedIn) return;
+
+        const userInfo = await me();
+        const { id } = userInfo;
+
+        // * 회원가입이 되어있는 경우
+        if (id === 4290865477) {
+          navigation.navigate("Home");
+        }
+        // * 회원가입이 되어있지 않은 경우
+        else {
+          navigation.navigate("Register");
+        }
+      })();
+
+      return;
+    }
+  }, [isLoading]);
 
   return (
     <View style={styles.container}>
