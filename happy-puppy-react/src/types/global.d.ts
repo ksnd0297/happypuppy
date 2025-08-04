eba8af921ab0791455b42998d225a19379854788
rare.nick <rare.nick@kakaocorp.com>
2025-08-04 15:56:08 +0900

    feat: 카맵용 타입 선언

--- happy-puppy-react/src/types/global.d.ts
+++ happy-puppy-react/src/types/global.d.ts
@@ -1,9 +1,15 @@
 export {};
 
+import {KakaoMaps} from './kakaoMap/kakaoMaps';
+
 declare global {
   interface Window {
     ReactNativeWebView?: {
       postMessage: (message: string) => void;
     };
+    kakao: {
+      maps: KakaoMaps;
+    };
+    receiveLocation(coords: {latitude: number; longitude: number}): void;
   }
 }
