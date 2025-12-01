694fd9d886cd2fb054ee8639169c13508933b5e8
joy.hwan <joy.hwan@kakaocorp.com>
2025-12-01 23:07:46 +0900

    feat : 더미데이터 추가

--- happy-puppy-react-native/app/hooks/chat/useMyChat.ts
+++ happy-puppy-react-native/app/hooks/chat/useMyChat.ts
@@ -11,12 +11,97 @@ type Props = {
   options?: UseQueryOptions<ChatResponse[]>;
 };
 
+const DUMMY_CHAT = [
+  {
+    id: 1,
+    status: "ON",
+    name: "동천역 강아지 산책",
+    meetDate: "2025-12-03",
+    meetTime: "14:00:00",
+    meetAt: "2025-12-03T14:00:00",
+    introduce: "동천역 앞 탄천에서 저녁에 소형, 중형견 산책하실 견주 분 구해요! 저희 진돗개랑 친구해요 ~",
+    tags: "#산책 #소형견 #중형견 #반려견 #동천 #강아지",
+  },
+  {
+    id: 1,
+    status: "ON",
+    name: "동천역 강아지 산책",
+    meetDate: "2025-12-03",
+    meetTime: "14:00:00",
+    meetAt: "2025-12-03T14:00:00",
+    introduce: "동천역 앞 탄천에서 저녁에 소형, 중형견 산책하실 견주 분 구해요! 저희 진돗개랑 친구해요 ~",
+    tags: "#산책 #소형견 #중형견 #반려견 #동천 #강아지",
+  },
+  {
+    id: 1,
+    status: "ON",
+    name: "동천역 강아지 산책",
+    meetDate: "2025-12-03",
+    meetTime: "14:00:00",
+    meetAt: "2025-12-03T14:00:00",
+    introduce: "동천역 앞 탄천에서 저녁에 소형, 중형견 산책하실 견주 분 구해요! 저희 진돗개랑 친구해요 ~",
+    tags: "#산책 #소형견 #중형견 #반려견 #동천 #강아지",
+  },
+  {
+    id: 1,
+    status: "ON",
+    name: "동천역 강아지 산책",
+    meetDate: "2025-12-03",
+    meetTime: "14:00:00",
+    meetAt: "2025-12-03T14:00:00",
+    introduce: "동천역 앞 탄천에서 저녁에 소형, 중형견 산책하실 견주 분 구해요! 저희 진돗개랑 친구해요 ~",
+    tags: "#산책 #소형견 #중형견 #반려견 #동천 #강아지",
+  },
+  {
+    id: 1,
+    status: "ON",
+    name: "동천역 강아지 산책",
+    meetDate: "2025-12-03",
+    meetTime: "14:00:00",
+    meetAt: "2025-12-03T14:00:00",
+    introduce: "동천역 앞 탄천에서 저녁에 소형, 중형견 산책하실 견주 분 구해요! 저희 진돗개랑 친구해요 ~",
+    tags: "#산책 #소형견 #중형견 #반려견 #동천 #강아지",
+  },
+  {
+    id: 1,
+    status: "ON",
+    name: "동천역 강아지 산책",
+    meetDate: "2025-12-03",
+    meetTime: "14:00:00",
+    meetAt: "2025-12-03T14:00:00",
+    introduce: "동천역 앞 탄천에서 저녁에 소형, 중형견 산책하실 견주 분 구해요! 저희 진돗개랑 친구해요 ~",
+    tags: "#산책 #소형견 #중형견 #반려견 #동천 #강아지",
+  },
+  {
+    id: 1,
+    status: "ON",
+    name: "동천역 강아지 산책",
+    meetDate: "2025-12-03",
+    meetTime: "14:00:00",
+    meetAt: "2025-12-03T14:00:00",
+    introduce: "동천역 앞 탄천에서 저녁에 소형, 중형견 산책하실 견주 분 구해요! 저희 진돗개랑 친구해요 ~",
+    tags: "#산책 #소형견 #중형견 #반려견 #동천 #강아지",
+  },
+  {
+    id: 1,
+    status: "ON",
+    name: "동천역 강아지 산책",
+    meetDate: "2025-12-03",
+    meetTime: "14:00:00",
+    meetAt: "2025-12-03T14:00:00",
+    introduce: "동천역 앞 탄천에서 저녁에 소형, 중형견 산책하실 견주 분 구해요! 저희 진돗개랑 친구해요 ~",
+    tags: "#산책 #소형견 #중형견 #반려견 #동천 #강아지",
+  },
+];
+
 const useMyChat = (props: Props) => {
   const { userId, options } = props;
 
   return useQuery({
     queryKey: myChatQueryKey(userId),
-    queryFn: userId ? () => myChatQueryFn(userId) : skipToken,
+    queryFn: () => {
+      return DUMMY_CHAT;
+    },
     ...options,
   });
 };
--- happy-puppy-react-native/app/hooks/place/useGetPlace.ts
+++ happy-puppy-react-native/app/hooks/place/useGetPlace.ts
@@ -1,10 +1,40 @@
-import { getPlace } from "@/app/services/place/place";
+// import { getPlace } from "@/app/services/place/place";
 import { skipToken, useQuery } from "@tanstack/react-query";
 import { Coordinate } from "../../screens/DefaultWebviewScreen";
+import { PlaceResponse } from "@/app/services/place/types";
 
 type Props = {
   location?: Coordinate;
 };
+
+const DUMMY_PLACE = [
+  {
+    id: 1,
+    name: "조이 테스트 장소",
+    placeType: "PARK",
+    address: "다우리빌 402호",
+    roadAddress: "용인시 수지구 동천동 877",
+    contact: "010-1234-5678",
+    homepage: "https://naver.com",
+    offDays: "연중무휴",
+    operationTime: "오전7시 - 오후10시",
+    latitude: "37.394775",
+    longitude: "127.1111583",
+  },
+  {
+    id: 2,
+    name: "조이 테스트 장소22222",
+    placeType: "PARK",
+    address: "다우리빌 402호",
+    roadAddress: "용인시 수지구 동천동 877",
+    contact: "010-1234-5678",
+    homepage: "https://naver.com",
+    offDays: "연중무휴",
+    operationTime: "오전7시 - 오후10시",
+    latitude: "37.394775",
+    longitude: "127.1121583",
+  },
+];
 
 const useGetPlace = (props: Props) => {
   const { location } = props;
@@ -17,9 +47,10 @@ const useGetPlace = (props: Props) => {
     queryKey: [location],
     queryFn: enabled
       ? async () => {
-          const response = getPlace({ latitude: latitude.toString(), longitude: longitude.toString(), radius: 10000 });
+          // const response = getPlace({ latitude: latitude.toString(), longitude: longitude.toString(), radius: 1000000 });
+          // return response;
 
-          return response;
+          return DUMMY_PLACE as PlaceResponse[];
         }
       : skipToken,
     enabled: !!location,
