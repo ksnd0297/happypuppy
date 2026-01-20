040128d5e45fe9e483d5ca5480506976a353be68
ksnd0297 <ksnd0297@gmail.com>
2026-01-20 22:36:02 +0900

    feat : 정보 수정

--- happy-puppy-react-native/app/RootStack.tsx
+++ happy-puppy-react-native/app/RootStack.tsx
@@ -34,8 +34,10 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 const RootStack = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Webview" component={DefaultWebviewScreen} />
       <Stack.Screen name="Login" component={LoginPage} />
+
+      <Stack.Screen name="Webview" component={DefaultWebviewScreen} />
+
       <Stack.Screen name="Home" component={HomePage} />
 
       <Stack.Screen name="Register" component={RegisterPage} />
--- happy-puppy-react-native/app/services/host.ts
+++ happy-puppy-react-native/app/services/host.ts
@@ -1,3 +1,1 @@
-import { Platform } from "react-native";
-
-export const API_HOST = Platform.OS === "android" ? "http://10.0.2.2:8080" : "http://localhost:8080";
+export const API_HOST = "https://hapypupy.com";
